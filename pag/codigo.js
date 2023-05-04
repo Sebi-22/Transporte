82d58d0d96e49ab5ffb7af0f8462f07f9cad3fdc
Sebi-22 <srotbard@hotmail.com>
2023-05-04 12:11:20 -0300

    Cambios

---
+++ pag/codigo.js
@@ -0,0 +1,4 @@
+num1= 6;
+num2= 10;
+resultado= num1 + num2;
+alert(resultado);
---
+++ pag/script.js
@@ -0,0 +1,6 @@
+let nombre = prompt("ingrese nombre");
+let apellido =prompt("ingrese apelldio");
+let resul = (nombre + apellido);
+alert(resul);
+
+
